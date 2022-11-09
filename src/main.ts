aedf6058024538ff4cdeee072cb525e934246366
VChet <17050347+VChet@users.noreply.github.com>
2022-11-10 01:52:48 +0400

    feat: add PWA support

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,20 @@ import App from "./App.vue";
 
 import "./assets/global.scss";
 
+import { useRegisterSW } from "virtual:pwa-register/vue";
+
+useRegisterSW({
+  immediate: true,
+  onRegistered(registration) {
+    if (registration) {
+      /* eslint-disable no-console */
+      console.log("Service worker registered");
+    }
+  },
+  onRegisterError(error) {
+    /* eslint-disable no-console */
+    console.error(error);
+  },
+});
+
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,44 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { VitePWA } from "vite-plugin-pwa";
+import type { VitePWAOptions } from "vite-plugin-pwa";
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  base: "/",
+  strategies: "injectManifest",
+  srcDir: "src",
+  filename: "sw.ts",
+  includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+  manifest: {
+    name: "Переводы в Грузию",
+    short_name: "Переводы в Грузию",
+    description: "Доступные способы денежных переводов в Грузию",
+    icons: [
+      {
+        src: "icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+  workbox: { sourcemap: true },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
